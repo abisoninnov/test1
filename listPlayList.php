448672375e057807fad9bb6839ce7082eeedbeeb
vdsgdfgfdg <abison@innovaturelabs.com>
2016-07-19 19:57:43 +0530

    change nl1
    dgfdgdg

---
+++ listPlayList.php
@@ -0,0 +1,14 @@
+<?php 
+$playListArray = array(
+    	'listArr'  => array(
+		'keyword'	=> 'english',
+                'datacount'   => '10',
+                'startRow'     => '0', 
+            ),
+);
+echo "<pre>"; print_r($playListArray); echo "</pre>";
+$jsonplayListArray = json_encode($playListArray);
+echo "<pre>"; echo $jsonplayListArray; echo "</pre>"; echo "</br>";
+$jsonDecodeArray = json_decode($jsonplayListArray);
+echo "<pre>"; print_r($jsonDecodeArray); echo "</pre>";
+?>
